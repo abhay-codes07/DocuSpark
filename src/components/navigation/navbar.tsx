6a0a907d2381db71470210b5f8c49117faf25861
abhay-codes07 <abhaysingh0293@gmail.com>
2026-02-28 23:34:08 +0530

    feat: create navbar shell component scaffold

---
+++ src/components/navigation/navbar.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { Container } from "@/components/layout";
+
+export function Navbar() {
+  return (
+    <header className="sticky top-0 z-40">
+      <Container>
+        <div className="flex h-16 items-center justify-between">Navbar</div>
+      </Container>
+    </header>
+  );
+}
